e605d1c8ce911a6ef4bef2ac73a6684232e65275
Roman Fick <yoursromanfick@gmail.com>
2019-06-24 17:38:05 +0200

    add playermove

---
+++ js/script.js
@@ -0,0 +1,9 @@
+printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
+
+var computerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+var playerMove;
+playerMove = 'papier';
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
